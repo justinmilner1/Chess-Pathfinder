b4e23130e32385c8ea2a0578644479f12f8d6176
Justin Milner <jjm7818@rit.edu>
2020-01-12 10:36:14 -0500

    trying

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDom from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+console.log("in index.js");
 ReactDom.render(<App />, document.getElementById("root"));
+console.log("after log index.js");
